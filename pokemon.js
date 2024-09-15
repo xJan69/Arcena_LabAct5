271e8275728e589fc481d3df0fe6b0099f3c0a4d
Jan <janarcena13@gmail.com>
2024-09-15 19:13:47 +0800

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,58 @@
+function _0x3b89(_0x28a820, _0x188ad2) {
+    const _0x57f9d9 = _0x57f9();
+    return _0x3b89 = function(_0x3b892d, _0x36ed7d) {
+        _0x3b892d = _0x3b892d - 0xec;
+        let _0x140a32 = _0x57f9d9[_0x3b892d];
+        return _0x140a32;
+    }
+    ,
+    _0x3b89(_0x28a820, _0x188ad2);
+}
+const _0x1e519e = _0x3b89;
+(function(_0x166602, _0x1b4b2b) {
+    const _0x55c385 = _0x3b89
+      , _0x40caf3 = _0x166602();
+    while (!![]) {
+        try {
+            const _0x9c1edc = parseInt(_0x55c385(0xfb)) / 0x1 * (-parseInt(_0x55c385(0xf8)) / 0x2) + parseInt(_0x55c385(0xef)) / 0x3 + -parseInt(_0x55c385(0xf2)) / 0x4 * (-parseInt(_0x55c385(0xf7)) / 0x5) + parseInt(_0x55c385(0xee)) / 0x6 + -parseInt(_0x55c385(0xf9)) / 0x7 + -parseInt(_0x55c385(0xfe)) / 0x8 + parseInt(_0x55c385(0xfd)) / 0x9 * (parseInt(_0x55c385(0x102)) / 0xa);
+            if (_0x9c1edc === _0x1b4b2b)
+                break;
+            else
+                _0x40caf3['push'](_0x40caf3['shift']());
+        } catch (_0x4526c0) {
+            _0x40caf3['push'](_0x40caf3['shift']());
+        }
+    }
+}(_0x57f9, 0x27515),
+$(document)[_0x1e519e(0xf3)](function() {
+    const _0x51efa2 = _0x1e519e;
+    fetch('pokedex.json')[_0x51efa2(0x101)](_0xb52c83=>_0xb52c83['json']())['then'](_0x3a4439=>{
+        const _0x3a0465 = _0x51efa2;
+        let _0x4df9c0 = 0x0;
+        _0x3a4439[_0x3a0465(0x100)](_0x3497b9=>{
+            const _0x2c1a84 = _0x3a0465;
+            let _0x3ea0ec = _0x3497b9['id']
+              , _0x372046 = _0x3497b9[_0x2c1a84(0xec)]['english']
+              , _0x39c27a = _0x3497b9['image'][_0x2c1a84(0xfc)]
+              , _0x60cb12 = _0x3497b9['type']
+              , _0x19e200 = '';
+            _0x60cb12['forEach'](_0x215680=>{
+                const _0x2fd8e2 = _0x2c1a84;
+                _0x19e200 += _0x2fd8e2(0xf4) + _0x215680 + '\x22>' + _0x215680 + _0x2fd8e2(0x103);
+            }
+            ),
+            $(_0x2c1a84(0xed))['append'](_0x2c1a84(0xff) + _0x39c27a + _0x2c1a84(0xf5) + _0x372046 + '\x22\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<ul\x20type=\x22none\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22pokemon-id\x22>#' + _0x3ea0ec + _0x2c1a84(0xf1) + _0x4df9c0++ + '\x22>' + _0x372046 + _0x2c1a84(0xf0) + _0x19e200 + _0x2c1a84(0xf6)),
+            console[_0x2c1a84(0xfa)](_0x19e200);
+        }
+        );
+    }
+    );
+}));
+function _0x57f9() {
+    const _0x355277 = ['<span\x20class=\x22', '\x22\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20alt=\x22', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</ul>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>', '100liqzxm', '4kjHnfh', '1547413vFkxws', 'log', '157433HGDMTC', 'hires', '54UXGdPC', '12904tbxWAL', '<div\x20class=\x22card\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20src=\x22', 'forEach', 'then', '81890VhoVFm', '</span>', 'name', '.pokemon-container', '1126998elYYBf', '490788MhCoLY', '</a>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22pokemon-type\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20', '</li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22pokemon-name\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22pages/pokemon.html?id=', '59604NsIVhR', 'ready'];
+    _0x57f9 = function() {
+        return _0x355277;
+    }
+    ;
+    return _0x57f9();
+}
