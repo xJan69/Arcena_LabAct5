9398367314f4f56d1af523c268d32be3f3802879
Jan <janarcena13@gmail.com>
2024-09-15 19:11:50 +0800

    script.js

--- script.js
+++ script.js
@@ -2,24 +2,35 @@ $(document).ready(function(){
     fetch("pokedex.json")
         .then((rawData) => rawData.json())
         .then(pokedex => {
-            
+            let linkId = 0;
             pokedex.forEach(pokemon => {
                 //Declaration of Properties
                 let id = pokemon["id"];
-                let name = pokemon["name"];
+                let name = pokemon["name"]["english"];//["japanese"];
                 let image = pokemon["image"]["hires"];
                 let types = pokemon["type"];
                 let typesHtml = "";
                 
                 types.forEach(type =>{
                     typesHtml += `<span class="${type}">${type}</span>`;
-
-                    console.log(name);
+                    // console.log(name);
                 });
+                
+                $('.pokemon-container').append(
+                    `<div class="card">
+                        <a href="pages/pokemon.html?=id=${linkId++}">
+                            <img src="${image}" alt="${name}">
+                        </a>
+                        <ul type="none">
+                        <li class="pokemon-id">#${id}</li>
+                        <li class="pokemon-name">
+                            <a href="pages/pokemon.html?=id=${linkId++}">${name}</a>
+                        </li>
+                        <li class="pokemon-type">${typesHtml}</li>
+                        </ul>
+                    </div>`
+                );
             });
-        })
-
-
-
-
+            console.log(id);
+        });
 });
