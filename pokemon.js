5032ba268c83db214de0099885e1cbbe7b23e3e1
Jan <janarcena13@gmail.com>
2024-09-26 08:52:53 +0800

    Add files via upload

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-$(( )=> {
+$(()=> {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get("id");
@@ -6,18 +6,19 @@ $(( )=> {
 
     fetch("pokedex.json")
     .then((rawData) => rawData.json())
-    .then((pokedex) => {
-        console.log(pokedex[id]);
+    .then(pokedex => {
+        
+        //console.log(pokedex);
         //Declaration of Properties
         let pokemon = pokedex[id];
-        let id = (pokemon["id"] + 10000).toString().substring(1);
+        let pokemonId = (pokemon["id"] + 10000).toString().substring(1);
         let name = pokemon["name"]["english"];
         let image = pokemon["image"]["hires"];
-        let description = pokemon["description"];
+        let desc = pokemon["description"];
         let types = pokemon["type"];
         let typesHtml = "";
         types.forEach((type) => {
-            typesHtml += `<span class="${types.toLowerCase()}">${type}</span>`;
+            typesHtml += `<span class="${type.toLowerCase()}">${type}</span>`;
         });
         let species = pokemon["species"];
         let height = pokemon["profile"]["height"];
@@ -25,14 +26,12 @@ $(( )=> {
         let abilities = pokemon["profile"]["ability"];
         let abilitiesHtml = "";
         abilities.forEach((ability) => {
-            abilitiesHtml += `<span> class="pokemon-ability">$<{ability[0]}</span>`;
+            abilitiesHtml += `<span class="pokemon-page-abilities"> ${ability[0]}</span>`;
         });
         //Display
         $(".pokemon-page-name").html(name);
-        $("pokemon-page-image").html(`<img src="${image}" alt="${name}"/>`);
-
-        //a1
-        getCompletion('.pokemon-page-description', name);
+        $(".pokemon-page-image").html(`<img src="${image}" alt="${name}"/>`);
+        $(".pokemon-page-description").html(desc);
 
         //Details
         $(".pokemon-page-id").html(pokemonId);
@@ -57,6 +56,19 @@ $(( )=> {
         let speed_per = (speed / 200) * 100;
         let total = hp + attack + defense + sp_attack + sp_defense + speed;
 
-    });
+        $(".hp-val").html(`<div>${hp}</div>`);
+        $(".attack-val").html(`<div>${attack}</div>`);
+        $(".defense-val").html(`<div>${defense}</div>`);
+        $(".sp-attack-val").html(`<div>${sp_attack}</div>`);
+        $(".sp-defense-val").html(`<div>${sp_defense}</div>`);
+        $(".speed-val").html(`<div>${speed}</div>`);
+        $(".total").html(`<div>${total}</div>`);
 
+        $(".hp div").animate({ width: (200 / 100) * speed_per }, 1000);
+        $(".attack div").animate({ width: (200 / 100) * attack_per }, 1000);
+        $(".defense div").animate({ width: (200 / 100) * defense_per }, 1000);
+        $(".sp-attack div").animate({ width: (200 / 100) * sp_attack_per }, 1000);
+        $(".sp-defense div").animate({ width: (200 / 100) * sp_defense_per }, 1000);
+        $(".speed div").animate({ width: (200 / 100) * speed_per }, 1000);
+    });
 });
